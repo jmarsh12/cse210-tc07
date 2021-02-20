6451c342b433432c33943f79a892febcedab1b1d
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-19 18:49:22 -0700

    added some todos

---
+++ speed/speed/game/buffer.py
@@ -0,0 +1,7 @@
+class Buffer:
+
+    def check_match(self, word):
+        # TODO check if word is anywhere in the buffer
+        # TODO if so, return True; if not, return False
+        pass
+    pass
