8684fbc7fc3d287bebe3ef68698b6dba621806cb
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-22 13:43:22 -0700

    added comments

--- speed/speed/game/location.py
+++ speed/speed/game/location.py
@@ -1,18 +1,24 @@
 class Location:
 
     def __init__(self, x, y):
-
+        """
+        The constructor method.
+        """
         self._x = x
         self._y = y
 
     # TODO add relationship between x and y, and word position
 
     def get_x(self):
+        """
+        This function is returning the x value.
+        """
         return self._x
 
     def get_y(self):
+        """
+        This function is returning the y value.
+        """
         return self._y
-
-
 
     pass
