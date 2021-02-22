c1ea4a626144eac148a73bd8bfd9d99a74c8e8ea
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-22 13:37:44 -0700

    stubbed out location

---
+++ speed/speed/game/location.py
@@ -0,0 +1,18 @@
+class Location:
+
+    def __init__(self, x, y):
+
+        self._x = x
+        self._y = y
+
+    # TODO add relationship between x and y, and word position
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
+
+
+
+    pass
