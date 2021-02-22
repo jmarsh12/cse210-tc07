a05c3b031b228079080a0db0b6ab6c30a219d53b
Bryson <bryson.berrett19@gmail.com>
2021-02-22 11:12:15 -0700

    Input service created

--- speed_template/speed/game/input_service.py
+++ speed_template/speed/game/input_service.py
@@ -2,7 +2,8 @@ import sys
 from asciimatics.event import KeyboardEvent
 
 class InputService:
-    """Detects player input. The responsibility of the class of objects is to detect player keypresses and translate them into a point representing a direction (or velocity).
+    """Detects player input. The responsibility of the class of objects is to 
+    get the player in put and then sends or returns it so that the buffer can use it.
 
     Stereotype: 
         Service Provider
@@ -12,29 +13,35 @@ class InputService:
     """
 
     def __init__(self, screen):
-        """The class constructor.
-        
-        Args:
-            self (InputService): An instance of InputService.
-        """
-        self._screen = screen
-        
-    def get_letter(self):
-        """Gets the letter that was typed. If the enter key was pressed returns an asterisk.
+        self._letters = ""
+    
+    def get_input(self):
+        self._letters = input(isalpha())
+        return self._letters
 
-        Args:
-            self (InputService): An instance of InputService.
+    #     """The class constructor.
+        
+    #     Args:
+    #         self (InputService): An instance of InputService.
+    #     """
+    #     self._screen = screen
+        
+    # def get_letter(self):
+    #     """Gets the letter that was typed. If the enter key was pressed returns an asterisk.
 
-        Returns:
-            string: The letter that was typed.
-        """
-        result = ""
-        event = self._screen.get_key()
-        if not event is None:
-            if event == 27:
-                sys.exit()
-            elif event == 10: 
-                result = "*"
-            elif event >= 97 and event <= 122: 
-                result = chr(event)
-        return result
+    #     Args:
+    #         self (InputService): An instance of InputService.
+
+    #     Returns:
+    #         string: The letter that was typed.
+    #     """
+    #     result = ""
+    #     event = self._screen.get_key()
+    #     if not event is None:
+    #         if event == 27:
+    #             sys.exit()
+    #         elif event == 10: 
+    #             result = "*"
+    #         elif event >= 97 and event <= 122: 
+    #             result = chr(event)
+    #     return result
