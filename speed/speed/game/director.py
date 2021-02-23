8eac77136afa238ed7b174978bed848675fa0874
purnsup <77720252+purnsup@users.noreply.github.com>
2021-02-22 23:33:24 -0700

    a

--- speed/speed/game/director.py
+++ speed/speed/game/director.py
@@ -115,8 +115,8 @@ class Director:
         # TODO draw actor: buffer
         # TODO go through each word and call draw_actor()
         self._output_service.clear_screen()
-        #self._output_service.draw_actor(self._buffer)
-        #self._output_service.draw_actor(self._score)
+        self._output_service.draw_actor(self._buffer)
+        self._output_service.draw_actor(self._score)
         self._output_service.draw_actors(self._words.get_segments())
         
         self._output_service.flush_buffer()
